eb3f0cbffb6e04997f22e3007a199a077f141048
ano <hugomarcw97@gmail.com>
2021-11-17 15:37:20 +0100

    started
    Many simple file

--- API/script/SQL/manga.sql
+++ API/script/SQL/manga.sql
@@ -14,79 +14,98 @@ DROP TABLE IF EXISTS manga CASCADE;
 
 CREATE TABLE user (
     id_user integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    login varchar(100),
-    pswd TEXT,
-    email varchar(100),
+    login varchar(100) NOT NULL,
+    pswd TEXT NOT NULL,
+    email varchar(100) NOT NULL,
     birthdate date default null,
     phone varchar(100) default null,
     profile_picture bytea,
-    is_Admin BOOLEAN
-    is_Admin BOOLEAN
+    is_Admin BOOLEAN DEFAULT false NOT NULL
 );
 
 CREATE TABLE manga (
     id_Manga integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    title varchar(100),
-    synopsis varchar(500),
-    new_price float,
-    type varchar(100),
-    sub_genre varchar(100),
-    autor varchar(100),
-    publisher varchar(100),
+    title varchar(100) NOT NULL,
+    synopsis varchar(500) NOT NULL,
+    new_price float NOT NULL,
+    type varchar(100) NOT NULL,
+    sub_genre varchar(100) NOT NULL,
+    autor varchar(100) NOT NULL,
+    publisher varchar(100) NOT NULL,
     main_picture bytea,
     is_finish BOOLEAN DEFAULT false NOT NULL
 );
 
 CREATE TABLE tome (
     id_tome integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    number integer default NOT NULL,
-    title varchar(100),
+    number integer NOT NULL,
+    title varchar(100) NOT NULL,
     picture bytea,
-    release_date TIMESTAMP,
-    FOREIGN KEY (fk_manga) REFERENCES manga (id_manga),
+    release_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (fk_manga) REFERENCES manga (id_manga) NOT NULL,
 );
 
 
 CREATE TABLE followed_manga (
     id_followed_manga integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    state integer CHECK  (state > 0 AND state < 4),
-    FOREIGN KEY (fk_manga) REFERENCES manga (id_manga),
-    FOREIGN KEY (fk_user) REFERENCES user (id_user),
+    state integer CHECK  (state > 0 AND state < 4) NOT NULL,
+    FOREIGN KEY (fk_manga) REFERENCES manga (id_manga) NOT NULL,
+    FOREIGN KEY (fk_user) REFERENCES user (id_user) NOT NULL,
 );
 
 CREATE TABLE readed_tome (
     id_reade_tome integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     read_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (fk_followed_manga) REFERENCES followed_manga (id_followed_manga),
-    FOREIGN KEY (fk_user) REFERENCES  user (id_user),
-    FOREIGN KEY (fk_tome) REFERENCES tome (id_tome),
+    FOREIGN KEY (fk_followed_manga) REFERENCES followed_manga (id_followed_manga) NOT NULL,
+    FOREIGN KEY (fk_user) REFERENCES  user (id_user) NOT NULL,
+    FOREIGN KEY (fk_tome) REFERENCES tome (id_tome) NOT NULL,
 );
 
 
 /*-------------------------INSERT pour débuter -----------------------------*/
 
 INSERT INTO user
-    (login, pswd, email, birthdate, phone, profile_picture, is_Admin)
+    (login, pswd, email, is_Admin)
     VALUES
-        ('admin', 'tigrou007', , '00000', ,true);
+        ('admin', 'tigrou007','test@gmail.com',true);
 
 INSERT INTO user
-    (login, pswd, email, birthdate, phone, profile_picture, is_Admin)
+    (login, pswd, email)
     VALUES
-        ('remy', 'remy007', , '00000', ,false);
+        ('remy', 'remy007', 'test@gmail.com');
 
 INSERT INTO user
-    (login, pswd, email, birthdate, phone, profile_picture, is_Admin)
+    (login, pswd, email)
     VALUES
-        ('hugo', 'hugo007', , '00000', ,false);
+        ('hugo', 'hugo007','test@gmail.com');
 
 INSERT INTO manga
-    (title,synopsis,new_price,type,sub_genre,autor,publisher,main_picture,is_finish)
+    (title,synopsis,new_price,type,sub_genre,autor,publisher,is_finish)
     VALUES
-        ('Bleach','synopsis',7.50,'shonen', 'Nekketsu', 'Tite Kubo','Glenat', ,true);
+        ('Bleach','synopsis',7.50,'shonen', 'Nekketsu', 'Tite Kubo','Glenat',true);
 
 INSERT INTO tome
-(number, title, picture, release_date)
+(number, title, release_date)
     VALUE
-    (1, 'The Death And The Strawberry',, 05/01/2002);
+    (1, 'The Death And The Strawberry', '19-05-2003 00:00:00');
 
+INSERT INTO tome
+(number, title, release_date)
+    VALUE
+    (2, 'Goodbye Parakeet, Goodnite My Sista', '28-07-2003 00:00:00');
+
+INSERT INTO tome
+(number, title, release_date)
+    VALUE
+    (3, 'Memories In The Rain', '15-10-2003 00:00:00');
+
+INSERT INTO tome
+(number, title, release_date)
+    VALUE
+    (4, 'Quincy Archer Hates You', '18-02-2004 00:00:00');
+
+
+INSERT INTO tome
+(number, title, release_date)
+    VALUE
+    (5, 'Rightarm Of The Giant', '21-04-2004 00:00:00');
